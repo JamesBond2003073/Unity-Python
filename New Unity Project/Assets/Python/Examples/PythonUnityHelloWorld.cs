15da5d7779e5533801cc34c75f251bbf691d9e91
JamesBond2003073 <54620315+JamesBond2003073@users.noreply.github.com>
2021-01-05 10:15:12 +0530

    Initial

---
+++ New Unity Project/Assets/Python/Examples/PythonUnityHelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Exodrifter.UnityPython.Examples
+{
+	public class PythonUnityHelloWorld : MonoBehaviour
+	{
+		void Start()
+		{
+			var engine = global::UnityPython.CreateEngine();
+			var scope = engine.CreateScope();
+
+			string code = "import UnityEngine\nUnityEngine.Debug.Log('Hello world!')\nstr = 'Hello world!'"; 
+		//	code += "UnityEngine.Debug.Log('Hello world!')";
+
+			var source = engine.CreateScriptSourceFromString(code);
+			source.Execute(scope);
+			Debug.Log(scope.GetVariable<string>("str"));
+		}
+	}
+}
